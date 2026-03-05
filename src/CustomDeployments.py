d6a6b8da3d80353c8cfb81d6794c41f1192f75a7
Davide Domini <davide.domini@unibo.it>
2026-03-05 16:36:08 +0100

    fix: pass correct data to create_node

--- src/CustomDeployments.py
+++ src/CustomDeployments.py
@@ -8,7 +8,7 @@ def multi_gaussian(simulator: Simulator, nodes_per_gaussian: int, gaussian_stati
         for _ in range(nodes_per_gaussian):
             x = random.gauss(centerX, stddev)
             y = random.gauss(centerY, stddev)
-            simulator.create_node((x,y), node_id)
+            simulator.create_node((x,y), None, node_id)
             node_id += 1
 
 def multi_grid(simulator: Simulator, grid_statistics: list, random_seed: int) -> None:
@@ -17,5 +17,5 @@ def multi_grid(simulator: Simulator, grid_statistics: list, random_seed: int) ->
     for (xs, ys, width, height, spacing) in grid_statistics:
         for x in range (xs, xs + width, spacing):
             for y in range (ys, ys + height, spacing):
-                simulator.create_node((x,y), node_id)
+                simulator.create_node((x,y), None, node_id)
                 node_id += 1
