18f3602b6fd6178d6a385f9531dd06ca2f7ff01f
davidedomini <davide.domini@unibo.it>
2026-04-02 11:49:18 +0200

    feat: add cnn for emnist

---
+++ src/learning/models.py
@@ -0,0 +1,25 @@
+from torch import nn
+import torch.nn.functional as F
+
+class CnnEMNIST(nn.Module):
+    def __init__(self, num_classes=27):
+        super().__init__()
+
+        self.features = nn.Sequential(
+            nn.Conv2d(1, 8, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+            nn.MaxPool2d(2)
+        )
+
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(8 * 7 * 7, 32),
+            nn.ReLU(),
+            nn.Linear(32, num_classes)
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = self.classifier(x)
+        return x
