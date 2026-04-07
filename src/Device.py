5b307bbdb44012ac746052810fdc5554deb9afc5
davidedomini <davide.domini@unibo.it>
2026-04-07 17:30:26 +0200

    feat: initialize each device with its own data, device 0 (which is the only movable device) has data for each subarea

--- src/Device.py
+++ src/Device.py
@@ -1,4 +1,5 @@
 from phyelds.calculus import aggregate
+from phyelds.libraries.device import local_id
 from phyelds.libraries.time import local_time
 from CustomLeaderElection import elect_leaders
 from phyelds.libraries.collect import count_nodes
@@ -9,14 +10,15 @@ from phyelds.simulator.deployments import deformed_lattice
 from phyelds.libraries.distances import neighbors_distances
 
 @aggregate
-def device():
-    """
-    Example to use the phyelds library to create a simple simulation
-    :return:
-    """
+def device(data, moving=False):
+
+    train_data, val_data = data.train_data
+    test_data = data.test_data[0]
+    other_data = {i + 1: d for i, d in enumerate(data.other_data)} if local_id() == 0 else {}
+
     distances = neighbors_distances()
     (am_i_leader, leader_id) = elect_leaders(20, distances)
-    # potential = distance_to(am_i_leader, distances)
-    # nodes = count_nodes(potential)
-    # area_value = broadcast(leader, nodes, distances)
+    potential = distance_to(am_i_leader, distances)
+    nodes = count_nodes(potential)
+    area_value = broadcast(leader, nodes, distances)
     return leader_id
--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 import random
 from Device import device
+from dataclasses import dataclass
+from torch.utils.data import Subset
 from CustomDeployments import multi_grid
 from phyelds.simulator import Simulator, Node
 from CustomRenderMonitor import CustomRenderMonitor
@@ -9,21 +11,35 @@ from phyelds.simulator.neighborhood import radius_neighborhood
 from phyelds.simulator.effects import DrawNodes, DrawEdges, RenderConfig, RenderMode
 from ProFed import download_dataset, split_train_validation, partition_to_subregions
 
+@dataclass
+class DeviceData:
+    train_data: Subset
+    test_data: Subset
+    other_data: list[tuple[Subset, Subset]]
+
+def seed_everything(seed: int) -> None:
+    random.seed(seed)
+    # TODO - seed also torch
+
 def move_node(simulator: Simulator, time_delta: float, node: Node, i: int, **kwargs) -> None:
     possible_positions = [(0,0), (30.5, 0.5), (30.5, 30.5), (0.5, 30.5)]
     node.update(new_position = possible_positions[i])
     simulator.schedule_event(time_delta, move_node, simulator, time_delta, node, (i+1)%4, **kwargs)
 
 def run_simulation(dataset_name: str, partitioning_method: str, number_of_regions: int, seed: int = 42) -> None:
-    random.seed(seed)
+    seed_everything(seed)
 
     simulator = Simulator()
-    # deformed lattice
+
+    ## Nodes deployment
     simulator.environment.set_neighborhood_function(radius_neighborhood(40))
-    mapping_area_nodes = multi_grid(simulator, [(0, 0, 5, 5, 1), (0, 30, 5, 5, 1), (30, 0, 5, 5, 1), (30, 30, 5, 5, 1)], 42)
+    mapping_area_nodes = multi_grid(simulator, [(0, 0, 7, 6, 2), (0, 30, 7, 6, 2), (30, 0, 7, 6, 2), (30, 30, 7, 6, 2)], 42)
+    nodes_per_subarea = len(mapping_area_nodes[0])
 
+    print(f'Nodes per subarea: {nodes_per_subarea}')
+
+    ## Data split and distribution
     train_data, test_data = download_dataset(dataset_name)
-
     train_data, validation_data = split_train_validation(train_data, 0.7)
     test_data, _ = split_train_validation(test_data, 1.0)
 
@@ -45,11 +61,55 @@ def run_simulation(dataset_name: str, partitioning_method: str, number_of_region
         seed,
     )
 
-    ## TODO - finish datasets setup
+    all_data = {}
+
+    for area_id in range(number_of_subareas):
+        train_mapping_device_data = train_environment.from_subregion_to_devices(
+            area_id,
+            nodes_per_subarea + (0 if area_id == 0 else 1),
+        )
+        test_mapping_device_data = test_environment.from_subregion_to_devices(
+            area_id,
+            nodes_per_subarea + (0 if area_id == 0 else 1),
+        )
+        all_data[area_id] = (train_mapping_device_data, test_mapping_device_data)
+        print(f'Area: {area_id} -- Number of devices: {len(train_mapping_device_data)} -- ids {train_mapping_device_data.keys()}')
+
+    device_data = {}
+
+    print(all_data.keys())
+    for area_id in mapping_area_nodes.keys():
+        ids = mapping_area_nodes[area_id]
+        for index in ids:
+            train_data_mapping, test_data_mapping = all_data[area_id]
+            train_data = train_data_mapping[index % nodes_per_subarea]
+            test_data = test_data_mapping[index % nodes_per_subarea]
+
+            if index == 0:
+                other_data = []
+                for next_area_id in range(1, number_of_subareas):
+                    train_data_mapping, test_data_mapping = all_data[next_area_id]
+                    next_train_data = train_data_mapping[nodes_per_subarea]
+                    next_test_data = test_data_mapping[nodes_per_subarea]
+                    other_data.append((next_train_data[0], next_train_data[1], next_test_data[0]))
+            else:
+                other_data = None
+
+            device_data[index] = DeviceData(train_data, test_data, other_data)
 
     # schedule the main function
     for node in simulator.environment.nodes.values():
-        simulator.schedule_event(random.random() / 100, aggregate_program_runner, simulator, 1.1, node, device)
+        moving = node.id == 0
+        simulator.schedule_event(
+            random.random() / 100,
+            aggregate_program_runner,
+            simulator,
+            1.1,
+            node,
+            device,
+            data=device_data[node.id],
+            moving=moving,
+        )
 
     moving_node = list(simulator.environment.nodes.values())[0]
     simulator.schedule_event(0.1, move_node, simulator, 30.0, moving_node, 1)
@@ -64,7 +124,7 @@ def run_simulation(dataset_name: str, partitioning_method: str, number_of_region
             dt=0.1
         )
     )
-    simulator.run(100)
+    simulator.run(1)
 
 if __name__ == '__main__':
 
